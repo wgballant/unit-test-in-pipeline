6ebfdb47870b23eb286d835655e81f1ef679907a
Unknown <william.ballantyne@gmail.com>
2021-04-29 17:12:38 -0700

    add invocation code and test

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,30 @@
-import json 
+from datetime import timezone
 import datetime
-def lambda_handler(event, context):
-    # TODO implement 
-    print(event)
-    data = {
-        'output': 'Hello from '+ event['Country'],
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
 
+import io
+import json
+import boto3
+from botocore.errorfactory import ClientError
+
+def lambda_handler(event, context):
+    dt = datetime.datetime.utcnow().isoformat()
+
+    client = boto3.client('s3')
+    str_value = ""
+    try:
+        bytes_buffer = io.BytesIO()
+        client.download_fileobj(Bucket='helmutcranium', Key='invocations.csv', Fileobj=bytes_buffer)
+        byte_value = bytes_buffer.getvalue()
+        str_value = byte_value.decode()
+    except ClientError:
+        pass
+
+    str_value += f'\r\n{dt}'
+
+    client.put_object(Body=str_value, Bucket='helmutcranium', Key='invocations.csv')
+
+    print(str_value)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello World!')
+    }
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -11,13 +11,7 @@ class TestHandlerCase(unittest.TestCase):
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello from '+event['Country'], result['body'])
-        event={'Country':'INDIA'}
-        result = lambda_function.lambda_handler(event, None)
-        print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello from '+event['Country'], result['body'])
+        self.assertIn('Hello World!', result['body'])
 
 
 if __name__ == '__main__':
